c6b14955abcaa07caa48d2ee824c90704dfe07dd
Alberto Xamin <alberto@xamin.it>
2018-03-27 11:52:32 +0200

    fix wrong assumptions

--- 27_3_18.R
+++ 27_3_18.R
@@ -6,16 +6,13 @@ tot_male <- 7
 tot_female <- 6
 
 #Domanda 1
-any_mf <- choose(tot_male, male) * choose(tot_female, female) *
-    factorial(male + female)
+any_mf <- choose(tot_male, male) * choose(tot_female, female)
 print_res("Tutti", any_mf)
 
 #Domanda 2
-picky_f <- choose(tot_male, male) * choose(tot_female - 1, female - 1) *
-    factorial(male + female)
+picky_f <- choose(tot_male, male) * choose(tot_female - 1, female - 1)
 print_res("Una certa femmina", picky_f)
 
 #Domanda 3
-picky_m <- choose(tot_male - 2, male) * choose(tot_female, female) *
-    factorial(male + female)
+picky_m <- choose(tot_male - 2, male) * choose(tot_female, female)
 print_res("Una certa femmina", picky_m)
